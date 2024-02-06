3f00cab4323e379d4c3c515285a8ef7a694545f8
M-Danish-J <imdanishjaved@gmail.com>
2024-02-06 18:57:36 +0500

    Update SideBar.tsx

--- app/components/SideBar.tsx
+++ app/components/SideBar.tsx
@@ -4,10 +4,10 @@ import React, { useState } from 'react';
 const SideBar = ({ setOnClick }: any) => {
     return (
         <div className="bg-gradient-to-b from-yellow-500 to-green-500 h-full flex flex-col justify-between w-full lg:w-64">
-            <div className="pt-8 pb-4 px-4 text-center">
+            <div className="pt-4 pb-2 px-4 text-center">
                 <p className='text-3xl font-bold text-white'>Our Menu</p>
             </div>
-            <div className="flex flex-wrap lg:flex-col items-center lg:space-y-2 pb-8">
+            <div className="flex flex-wrap lg:flex-col items-center lg:space-y-1 pb-8">
                 <Tab name='Pizza' onClick={() => setOnClick('Pizza')} />
                 <Tab name='Burger' onClick={() => setOnClick('Burger')} />
                 <Tab name='Sandwich and Warps' onClick={() => setOnClick('Sandwich and Warps')} />
@@ -18,7 +18,7 @@ const SideBar = ({ setOnClick }: any) => {
                 <Tab name='Paratha Roll and Grill' onClick={() => setOnClick('Paratha Roll and Grill')} />
                 <Tab name='Pizza Deals' onClick={() => setOnClick('Pizza Deals')} />
             </div>
-            <div onClick={() => setOnClick('History')} className="lg:w-40 py-2 px-4 rounded-md cursor-pointer font-bold text-center text-white transition duration-100 transform hover:scale-105 hover:shadow-lg border mx-auto my-3">
+            <div onClick={() => setOnClick('History')} className="lg:w-40 py-2 px-4 rounded-md cursor-pointer font-bold text-center text-white transition duration-100 transform hover:scale-105 hover:shadow-lg border mx-auto mb-3">
                 Order History
             </div>
         </div>
